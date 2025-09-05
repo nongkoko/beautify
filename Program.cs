0df42098c29d5240e3443d6fe57cb3f71286d9f2
tukangPES <afryanto.livan@outlook.com>
2025-09-05 09:40:31 +0700

    initial data

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Text.Json;
+
+Console.WriteLine("Hello, World!");
+
+var stringFromPipeline = Console.In.ReadToEndAsync();
+using JsonDocument doc = JsonDocument.Parse(await stringFromPipeline);
+var root = doc.RootElement;
+var options = new JsonSerializerOptions
+{
+    WriteIndented = true
+};
+string prettyJson = JsonSerializer.Serialize(root, options);
+Console.WriteLine(prettyJson);
